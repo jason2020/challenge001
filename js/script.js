4f160afc140a1d17b636af5baa39573f182c7699
jason2020 <jtay20@punahou.edu>
2016-06-15 14:57:54 -1000

    Changed the font of the bottom text and added some javascript, which changes the height of the top div.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// hi my name is jason
+var flipped = false;
+
+$("#surround").click(function(){
+	if (flipped == false) {
+		$("#arrow").css("-webkit-transform", "scaleY(-1)");
+		$("#main").animate({height: "40vh"});
+		flipped = true;
+	} else if (flipped == true) {
+		$("#arrow").css("-webkit-transform", "scaleY(1)");
+		$("#main").animate({height: "55vh"});
+		flipped = false;
+	}
+});
